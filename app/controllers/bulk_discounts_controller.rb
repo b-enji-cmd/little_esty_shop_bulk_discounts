e137c8b96ab37da08b2d4d01e43d65f30a8a662e
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-04 19:08:44 -0700

    Write create action for BD

--- app/controllers/bulk_discounts_controller.rb
+++ app/controllers/bulk_discounts_controller.rb
@@ -1,6 +1,7 @@
 class BulkDiscountsController < ApplicationController
   def index
   	@merchant = Merchant.find(params[:merchant_id])
+  	binding.pry
   end
 
   def show
@@ -10,5 +11,27 @@ class BulkDiscountsController < ApplicationController
   end
 
   def edit
+  end
+
+  def new
+  	BulkDiscount.new
+  end
+
+  def create
+  	@merchant = Merchant.find(params[:merchant_id])
+  	@discount = BulkDiscount.new(bulk_discount_params)
+    binding.pry
+  	if @discount.save
+  		redirect_to merchant_bulk_discount_path(@merchant)
+  	else
+  		flash[:notice] = 'Error creating discount, please fill in all fields correctly'
+  		render :new
+  	end
+  end
+
+  private
+
+  def bulk_discount_params
+  	params.permit(:quantity_threshold, :percentage_discount, :merchant_id)
   end
 end
