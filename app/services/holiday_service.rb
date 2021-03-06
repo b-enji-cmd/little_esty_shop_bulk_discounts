fc79684b88ede25b54262ac29ac737a190e6b54d
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-06 11:56:15 -0700

    Write API usage

--- app/services/holiday_service.rb
+++ app/services/holiday_service.rb
@@ -1,7 +1,10 @@
 class HolidayService < ApiService
 	def self.holidays
 		endpoint = 'https://date.nager.at/Api/v2/NextPublicHolidays/us'
-		parsed = get_data(endpoint)
-		
+		parsed = get_data(endpoint)[0..2]
+
+		parsed.map do |holiday|
+			Holiday.new(holiday)
+		end
 	end
 end
