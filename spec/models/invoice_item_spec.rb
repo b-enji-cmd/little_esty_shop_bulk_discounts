8dc99346fb05072cfa2712c2f392a4310cb5e33f
Ben Fulton <benjamin.fulton99@gmail.com>
2021-03-09 13:40:45 -0700

    Include relationship testing

--- spec/models/invoice_item_spec.rb
+++ spec/models/invoice_item_spec.rb
@@ -11,5 +11,6 @@ RSpec.describe InvoiceItem, type: :model do
   describe "relationships" do
     it { should belong_to :invoice }
     it { should belong_to :item }
+    it { should have_many(:bulk_discounts).through(:invoice) }
   end
 end
